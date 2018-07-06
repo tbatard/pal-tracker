a8c26da28065f0131c44e2a16969b35f0e0d2c06
Tony Batard <tbatard@pivotal.io>
2018-07-05 18:10:55 -0700

    Fix tests and add manifest

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,30 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+    private Map<String, String> env = new HashMap<>();
+
+    public EnvController(
+            @Value("${PORT:NOT SET}") String port,
+            @Value("${MEMORY_LIMIT:NOT SET}") String memoryLimit,
+            @Value("${CF_INSTANCE_INDEX:NOT SET}") String cfInstanceIndex,
+            @Value("${CF_INSTANCE_ADDR:NOT SET}") String cfInstanceAddr
+    ) {
+        env.put("PORT", port);
+        env.put("MEMORY_LIMIT", memoryLimit);
+        env.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+        env.put("CF_INSTANCE_ADDR", cfInstanceAddr);
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+        return env;
+    }
+}
